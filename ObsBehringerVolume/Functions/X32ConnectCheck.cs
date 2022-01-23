948254b6da2b15e82d72403fe3df72547ba74f2b
Maxim Kolpakov <max.kolpakov.st@gmail.com>
2022-01-23 17:50:21 +0300

    On branch main

---
+++ ObsBehringerVolume/Functions/X32ConnectCheck.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ObsBehringerVolume.Functions
+{
+    internal class X32ConnectCheck
+    {
+        public event EventHandler<bool> Connect;
+
+        private byte[] _buffer = new byte[256]; //Buffer
+        private Byte[] _sendBytes;
+        private int _sleep;
+
+        private Socket _socket;
+
+        public X32ConnectCheck(string ipAdress, int Port, int Timeout = 1000, int Sleep = 3000)
+        {
+            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp)
+            {
+                ReceiveTimeout = Timeout
+            };
+            _sleep = Sleep;
+
+            IPAddress mixerAddr = IPAddress.Parse(ipAdress);
+            IPEndPoint endPoint = new IPEndPoint(mixerAddr, Port);
+            _sendBytes = Encoding.ASCII.GetBytes("/status");
+            Task.Run(() => { CheckConnectMixer(endPoint); });
+        }
+
+        private void CheckConnectMixer(IPEndPoint endPoint)
+        {
+            while (true)
+            {
+                _socket.SendTo(_sendBytes, endPoint);
+                try
+                {
+                    _socket.Receive(_buffer);
+                    Connect?.Invoke(this, true);
+                    string recive = Encoding.UTF8.GetString(_buffer);
+                }
+                catch(Exception ex)
+                {
+                    Connect?.Invoke(this, false);
+                    Console.WriteLine($"Error : {ex.Message}");
+                }
+                Thread.Sleep(_sleep);
+            }
+        }
+    }
+}
--- ObsBehringerVolume/Program.cs
+++ ObsBehringerVolume/Program.cs
@@ -7,21 +7,35 @@ using System.Threading.Tasks;
 using OBSWebsocketDotNet;
 using OBSWebsocketDotNet.Types;
 using Newtonsoft.Json;
+using ObsBehringerVolume.Functions;
+using Behringer.X32;
+using System.Net;
 
 namespace ObsBehringerVolume
 {
     class Program
     {
         static protected OBSWebsocket obs;
+        static protected X32ConnectCheck x32check;
+        static protected X32Console console;
         static void Main(string[] args)
         {
+            x32check = new X32ConnectCheck("192.168.31.172", 10023);
+            x32check.Connect += X32check_Connect;
+            console = new X32Console() { IPAddress = IPAddress.Parse("192.168.31.172"), Port = 10023, Interval = 1000 };
             /*obs = new OBSWebsocket();
             obs.Connected += Obs_Connected;
             obs.Connect("ws://127.0.0.1:4444", "");
+            */
             while (true)
             {
                 Thread.Sleep(100);
-            }*/
+            }
+        }
+
+        private static void X32check_Connect(object sender, bool e)
+        {
+            Console.WriteLine($"Connect: {e}");
         }
 
         private static void Obs_Connected(object sender, EventArgs e)
